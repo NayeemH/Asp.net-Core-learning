bd980b9096af7afccb5141a6932c878bf7f25d2d
NayeemH <Nayeemh67@gmail.com>
2024-08-21 03:24:47 +0600

    add migration , db context and some controllers

---
+++ Nazwalks/NZWalks/NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:port/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // Get: https://localhost:port/api/students 
+        [HttpGet]
+        public IActionResult GetAllStudent()
+        {
+            string[] studentNames = new string[] { "John", "Jane", "Mark", "Emily", "David" };
+
+            return Ok(studentNames);
+        }
+    }
+}
